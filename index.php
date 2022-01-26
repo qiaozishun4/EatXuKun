69c595269e84d1d758f94ac2b4c98e41ce7c61c4
Asankilp <60691961+Asankilp@users.noreply.github.com>
2022-01-26 21:25:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
             <button type="button" class="btn btn-secondary btn-lg" onclick="replayBtn()">重来</button>
 			<button type="button" class="btn btn-secondary btn-lg" onclick="goRank();">排行</button>
             <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='https://github.com/Asankilp/EatXuKun'">开源</button>
+            <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='https://github.com/arcxingye/EatKano'">开源（原版）</button>
         </div>
     </div>
     </div>
